eaa6717d4591c67a5ae7e52a016003c495deb170
Asanbai Ortosaev <asanbaiortosaev@gmail.com>
2022-03-20 11:02:32 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        Generics метод тузунуз.Параметрине generic тибиндеги массивди алсын.
+        Ал метод биринчи массивдин биринчи жана акыркы элементтерин консолго чыгарсын,
+         анан бардык элементтерин чыгарсын.
+         */
+
+        ArrayList <Integer> integerArrayList = new ArrayList<>();
+        integerArrayList.add(5);
+        integerArrayList.add(6);
+        integerArrayList.add(7);
+        integerArrayList.add(8);
+
+        genericMethod(integerArrayList);
+
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("Hello");
+        stringArrayList.add("World");
+        stringArrayList.add("Java");
+        stringArrayList.add("Rules");
+
+        genericMethod(stringArrayList);
+
+    }
+
+    public static <T> void genericMethod(ArrayList<T> arrayList){
+        System.out.println("First element of arraylist: "+arrayList.get(0));
+        System.out.println("Last element of arraylist: "+arrayList.get(arrayList.size()-1));
+
+        for (T t : arrayList) {
+            System.out.println(t);
+        }
+    }
+}
+
